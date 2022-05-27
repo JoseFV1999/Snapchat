10ec61cb030f6f0274edd9bdc6a8eb8f46864052
JoseFV1999 <jose.falconi@tecsup.edu.pe>
2022-05-26 18:19:28 -0700

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Jose Falconi on 5/26/22.
+//  Copyright © 2022 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
